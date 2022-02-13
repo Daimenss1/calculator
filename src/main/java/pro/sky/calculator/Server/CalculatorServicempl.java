cc8d6559ea0e4bbb731c37bd12162728920c6176
dgobozov <saniamit@mail.ru>
2022-02-13 22:02:59 +0300

    ДЗ SPRING WEB (/CALCULATOR)

---
+++ src/main/java/pro/sky/calculator/Server/CalculatorServicempl.java
@@ -0,0 +1,27 @@
+package pro.sky.calculator.Server;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServicempl implements CalculatorService {
+
+    @Override
+    public int plus(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    @Override
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public double divide(int num1, int num2) {
+        return (double) num1/ num2;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+}
